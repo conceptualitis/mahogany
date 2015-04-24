9ff0ddc5e2e2a4c86e8395751e4adf08b592c6b6
Neil Duffy <neiladuffy@gmail.com>
2015-04-24 14:50:27 -0500

    cleaning up some, using the API to manipulate important

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('mahogany', function () {
     return function (css) {
         css.eachDecl(function (decl) {
-            decl.value = decl.value + ' !important';
+            decl.important = true;
         });
     };
 });
--- test/test.js
+++ test/test.js
@@ -15,4 +15,9 @@ describe('mahogany', function () {
         test('a { color: red; font-size: 12px; } b { font-weight: 400; }',
              'a { color: red !important; font-size: 12px !important; } b { font-weight: 400 !important; }', done);
     });
+
+    it('much transform, too much important', function (done) {
+        test('a { color: red !important; } b { font-weight: 400; }',
+             'a { color: red !important; } b { font-weight: 400 !important; }', done);
+    });
 });
