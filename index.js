af30dbaa4a10d02277488a56577e4372c6cad8d8
Neil Duffy <neiladuffy@gmail.com>
2016-02-19 08:35:32 -0600

    moving completely to 2 wide indentations

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('mahogany', function () {
-    return function (css) {
-        css.walkDecls(function (decl) {
-            decl.important = true;
-        });
-    };
+  return function (css) {
+    css.walkDecls(function (decl) {
+        decl.important = true;
+    });
+  };
 });
